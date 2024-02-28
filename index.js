c20e68762d6eb81dd204c234328b1fdaf2ac2197
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-02-28 15:42:12 +0100

    Exo 1 done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
+let hacker1 = "Gwen"
+console.log(`The driver's name is ${hacker1}`);
 
-
+let hacker2 = "Chris"
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
+
+
 
 
 // Iteration 3: Loops
